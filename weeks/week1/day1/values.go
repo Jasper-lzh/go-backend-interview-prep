4a3d526fa63db635948e7992dd39756e539ef133
zhaohui_li <zhaouhui.li@dell.com>
2025-12-29 08:16:55 +0000

    day1

---
+++ weeks/week1/day1/values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+   fmt.Println("go" + "lang")  // string
+
+   fmt.Println("1+1 =", 1+1) // int
+   fmt.Println("7.0/3.0", 7.0/3.0) // float
+
+   fmt.Println(true && false) // bool
+   fmt.Println(true || false)
+   fmt.Println(!true)
+}
